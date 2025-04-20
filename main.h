2233584e2dfa6f5d0b98cd09870981a35dfbd655
Simn-popo <paulin.simon@live.fr>
2025-04-20 20:53:08 +0200

    fix: environ

--- main.h
+++ main.h
@@ -11,7 +11,7 @@ extern char **environ;
 void parse_command(char *command, char **args, char *delim);
 void execute_command(char **args);
 int return_exe(char **args, char **env);
-int handle_builtin(char **args, char **env);
+int handle_builtin(char **args);
 char *_getenv(const char *name, char **env);
 void print_env(char **env);
 
