ee7de8dfa1202cdc67ce6479b7c14b14d172dea6
Simn-popo <paulin.simon@live.fr>
2025-04-20 14:12:01 +0200

    add :return exit 127

--- main.c
+++ main.c
@@ -7,11 +7,10 @@
 
 int main(void)
 {
-	char *command = NULL;
+	char *command = NULL, *args[1024], *delim = " ";
 	size_t len = 0;
-	char *args[1024];
 	ssize_t nread;
-	char *delim = " ";
+	int exit_status = 0;
 
 	while (1)
 	{
@@ -19,29 +18,31 @@ int main(void)
 			printf("Hugo is the best SWE, i agree: ");
 
 		nread = getline(&command, &len, stdin);
-
 		if (nread == -1)
 		{
 			if (isatty(STDIN_FILENO))
 				printf("\n");
 			break;
 		}
+
 		command[strcspn(command, "\n")] = '\0';
-
 		parse_command(command, args, delim);
-
 		if (args[0] == NULL)
 			continue;
 
-		if (strchr(args[0], '/') != NULL)
+		if (strchr(args[0], '/'))
 			execute_command(args);
-
 		else if (return_exe(args) == 0)
 		{
 			execute_command(args);
 			free(args[0]);
 		}
+		else
+		{
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			exit_status = 127;
+		}
 	}
 	free(command);
-	return (0);
+	return (exit_status);
 }
