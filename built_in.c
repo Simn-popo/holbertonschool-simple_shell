5777cb87065744f02dfb39627c93cf9312db8c09
Simn-popo <paulin.simon@live.fr>
2025-04-20 17:40:01 +0200

    change: name

---
+++ built_in.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_env - display env variable
+ */
+
+void print_env(void)
+{
+	int i = 0;
+
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+}
