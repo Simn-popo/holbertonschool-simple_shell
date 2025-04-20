3f65b6b62ba4b73afdd5a960e4f51cea55c644b4
Simn-popo <paulin.simon@live.fr>
2025-04-20 12:36:37 +0200

    fix conflit

--- _getenv.c
+++ _getenv.c
@@ -8,34 +8,14 @@
  */
 char *_getenv(const char *name, char **env)
 {
-<<<<<<< HEAD
-	int i, j, status;
-
-	for (i = 0; env[i] != NULL; i++)
-	{
-		status = 1;
-
-		for (j = 0; env[i][j] != '='; j++)
-		{
-			if (name[j] != env[i][j])
-			{
-				status = 0;
-				break;
-			}
-		}
-		if (status)
-		{
-			return (&env[i][j + 1]);
-=======
 	int i;
 	size_t len = strlen(name);
 
 	for (i = 0; env[i] != NULL; i++)
 	{
-		if(strncmp(env[i], name, len) == 0 && env[i][len] == '=')
+		if (strncmp(env[i], name, len) == 0 && env[i][len] == '=')
 		{
 			return (&env[i][len + 1]);
->>>>>>> simon
 		}
 	}
 	return (NULL);
