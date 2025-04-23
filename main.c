5a61d7311cf3699293b0496bfc38ff40b25c85a3
Simn-popo <paulin.simon@live.fr>
2025-04-24 00:23:00 +0200

    Restore old version

--- main.c
+++ main.c
@@ -7,34 +7,38 @@
  * Return: The exit status of the last executed command
  */
 
-int main(int ac, char **env)
+int main(int ac, char **av)
 {
 	char *ciao = NULL, *args[1024];
-	char *copy_env[1024];
 	size_t len = 0;
 	ssize_t nread;
 	int exit_status = 0;
 
 	(void)ac;
-	while (1) 
+	(void)av;
+
+	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			printf("Hugo and Fabien is the best SWE, I agree: ");
+			printf("Hugo is the best SWE, I agree: ");
 		nread = getline(&ciao, &len, stdin);
 		if (nread == -1)
 			break;
+
 		ciao[strcspn(ciao, "\n")] = '\0';
 		exit_shell(ciao, exit_status);
 		parse_command(ciao, args, " ");
+
 		if (args[0] == NULL)
 			continue;
-		if (handle_env(args, env, &exit_status, copy_env))
-			continue;
-		if (strchr(args[0], '/') != NULL)/* if cmd get '/' absolute path */
-			exit_status = execute_command(args, env);
-		else if (return_exe(args, env) == 0)/* looking in path */
+
+		if (strchr(args[0], '/') != NULL)
 		{
-			exit_status = execute_command(args, env);
+			execute_command(args, environ);
+		}
+		else if (return_exe(args, environ) == 0)
+		{
+			execute_command(args, environ);
 			free(args[0]);
 		}
 		else
