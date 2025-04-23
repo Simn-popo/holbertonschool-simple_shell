3b8a109b7adedd92bd9361d5e040013979c44250
Simn-popo <paulin.simon@live.fr>
2025-04-23 20:41:28 +0200

    Add: function handle and try to get path env whit debug check

--- built_in.c
+++ built_in.c
@@ -11,8 +11,17 @@ void print_env(char **env)
 {
 	int i = 0;
 
+	if (env == NULL)
+	{
+		printf("error : env is null\n");
+		return;
+	}
+	
+	printf("DEBUG: Printing environment variables...\n");
+
 	while (env[i] != NULL)
 	{
+		printf("env[%d] = %s\n", i, env[i]);
 		printf("%s\n", env[i]);
 		i++;
 	}
@@ -25,9 +34,25 @@ void print_env(char **env)
 
 void exit_shell (char *ciao, int exit_status)
 {
-	if (strcmp(ciao, "exit") == 0)
+	if (ciao != NULL && strcmp(ciao, "exit") == 0)
 	{
 		free(ciao);
 		exit(exit_status);
 	}
+}
+
+int handle_env(char **args, char **env, int *exit_status)
+{
+	if (args[0] != NULL && strcmp(args[0], "env") == 0)
+	{
+		printf("DEBUG: env command detected\n");
+		print_env(env);
+		*exit_status = 0;
+		return(1);
+	}
+	if (args[0] != NULL && strcmp(args[0], "exit") == 0)
+	{
+		exit_shell(args[0], *exit_status);
+	}
+	return(0);
 }
