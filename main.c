6163bdd1655a9271254e2970066808b7d59b2d0c
Simn-popo <paulin.simon@live.fr>
2025-04-23 20:44:11 +0200

    add:handel_env

--- main.c
+++ main.c
@@ -15,30 +15,25 @@ int main(int ac, char **env)
 	int exit_status = 0;
 
 	(void)ac;
-	(void)env;
-
-	while (1)
+	while (1) 
 	{
 		if (isatty(STDIN_FILENO))
 			printf("Hugo and Fabien is the best SWE, I agree: ");
 		nread = getline(&ciao, &len, stdin);
 		if (nread == -1)
 			break;
-
 		ciao[strcspn(ciao, "\n")] = '\0';
 		exit_shell(ciao, exit_status);
 		parse_command(ciao, args, " ");
-
 		if (args[0] == NULL)
 			continue;
-
+		if (handle_env(args, env, &exit_status))
+			continue;
 		if (strchr(args[0], '/') != NULL)/* if cmd get '/' absolute path */
+			exit_status = execute_command(args, env);
+		else if (return_exe(args, env) == 0)/* looking in path */
 		{
-			exit_status = execute_command(args, environ);
-		}
-		else if (return_exe(args, environ) == 0)/* looking in path */
-		{
-			exit_status = execute_command(args, environ);
+			exit_status = execute_command(args, env);
 			free(args[0]);
 		}
 		else
